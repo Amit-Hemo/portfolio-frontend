ec8491fa0506da96906faf4cf38d26d935454bdd
Amit Hemo <itsamit442@gmail.com>
2023-11-26 23:09:52 +0200

    chore: initial setup
    create everything needed for running a react app using vite, typescript and eslint
    
    #1

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1>Amit Hemo</h1>
+      <div className='card'>
+        <button onClick={() => alert('hello, server will be created later')}>check server</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
